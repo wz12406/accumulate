7faffc3d2ea3df57cc62276c8fe524abe908b930
wangzhen <768834204@qq.com>
2017-09-20 15:39:24 +0800

    项目结构调整，调整为父子工程

---
+++ algorithm/src/main/java/cn/wz/algorithm/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.wz.algorithm;
+
+/**
+ * @author Administrator
+ * @date 2017/9/20 15:30
+ * @desc
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("sadsadsa");
+    }
+}
